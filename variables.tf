3b2ef1828208e6546faf1b0ff20e8cd685580d3b
Simon Wilkins <sw2187@cam.ac.uk>
2025-05-02 10:26:33 +0100

    Fix tf formatting

--- variables.tf
+++ variables.tf
@@ -30,7 +30,7 @@ variable "public_subnets" {
 }
 
 variable "environment" {
-  type = string
+  type        = string
   description = "(Optional) Environment of all resources"
-  default = "development"
+  default     = "development"
 }
